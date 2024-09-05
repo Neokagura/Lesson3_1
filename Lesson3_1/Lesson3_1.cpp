373bc752316d5f0cab04d2ce3c032101f31da4c0
Ness <bur>
2024-09-05 18:01:54 +0300

    Добавьте файлы проекта.

---
+++ Lesson3_1/Lesson3_1.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    int firstNumber;
+    int secondNumber;
+
+    cout << "Введите первое число:\n";
+    cin >> firstNumber;
+    cout << "Введите второе число:\n";
+    cin >> secondNumber;
+    cout << "-----Проверяем-----\n";
+    if (firstNumber > secondNumber) {
+        cout << "Наименьшее число: " << secondNumber << endl;
+
+    }
+    else if (secondNumber > firstNumber) {
+        cout << "Наименьшее число: " << firstNumber << endl;
+    }
+    else {
+        cout << "Числа равны";
+    }
+}
+
